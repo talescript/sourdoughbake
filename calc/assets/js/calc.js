a05aefe4934af59c325db2ee82f2a9433d66d5c0
Alexader <anthomelanous@hotmal.com>
2018-10-21 13:54:12 -0500

    Add flour calc for autolyse and error checking if over

--- calc/assets/js/calc.js
+++ calc/assets/js/calc.js
@@ -5,12 +5,16 @@ var autolyse = form.elements["autolyse"];
 var starter = form.elements["starter"]
 var salt = form.elements["salt"]
 var water = form.elements["water"]
+var flourOne = form.elements["flourOne"];
+var flourTwo = form.elements["flourTwo"];
 
 var autolyse_r = document.querySelector("#autolyse_r");
 var starter_r = document.querySelector("#starter_r");
 var salt_r = document.querySelector("#salt_r");
 var water_r = document.querySelector("#water_r");
 var dw = document.querySelector(".dw");
+var flourOne_r = document.querySelector("#flourOne_r");
+var flourTwo_r = document.querySelector("#flourTwo_r");
 
 form.elements["autolyse"].addEventListener('input', getValue);
 form.elements["starter"].addEventListener('input', getValue);
@@ -31,12 +35,20 @@ function getValue(){
     else if (this.id == "water") {
         water.value = this.value;        
     }
+    else if (this.id == "flourOne") {
+        flourOne.value = this.value;
+    }
+    else if (this.id == "flourTwo") {
+        flourTwo.value = this.value;
+    }
 }
 
 var btn = document.querySelector(".calculate");
 btn.addEventListener("click", calculate);
 
 function calculate(){
+
+    // initialize variables
     var autolyseWeight = autolyse.value * 1;
     var starterWeight = autolyseWeight * starter.value;
     var halfStarter = starterWeight / 2
@@ -44,9 +56,29 @@ function calculate(){
     var saltWeight = doughWeight * salt.value;
     var waterWeight = (doughWeight * water.value) - halfStarter;
 
-    autolyse_r.textContent = autolyseWeight.toFixed(2);
-    starter_r.textContent = starterWeight.toFixed(2);
-    salt_r.textContent = saltWeight.toFixed(2);
-    water_r.textContent = waterWeight.toFixed(2);
-    dw.textContent = doughWeight;
+    var flourOneWeight = autolyseWeight * flourOne.value;
+    var flourTwoWeight = autolyseWeight * flourTwo.value;
+    console.log(flourOneWeight);
+    console.log(flourTwoWeight);
+
+    // place results on table
+    autolyse_r.textContent = autolyseWeight.toFixed(2) + "g";
+    starter_r.textContent = starterWeight.toFixed(2) + "g";
+    salt_r.textContent = saltWeight.toFixed(2) + "g";
+    water_r.textContent = waterWeight.toFixed(2) + "g";
+    dw.textContent = doughWeight + "g";
+    flourOne_r.textContent = flourOneWeight.toFixed(2) + "g"
+    flourTwo_r.textContent = flourTwoWeight.toFixed(2) + "g"
+
+    // Check if the flours are less or equal to autolyse
+    if (flourOneWeight + flourTwoWeight > autolyseWeight){
+        flourOne_r.classList.add("bg-danger", "text-white");
+        flourTwo_r.classList.add("bg-danger", "text-white");
+
+    }
+    else {
+        flourOne_r.classList.remove("bg-danger", "text-white");
+        flourTwo_r.classList.remove("bg-danger", "text-white");
+    }
+
 }
